ee089437301a8e24aa950b58ddcc589e0e3116dd
gwj <1499801909@qq.com>
2022-01-17 17:03:48 +0800

    【说明】自定义view——使用paint和canvas绘制简单形状

--- app/src/main/java/com/example/customview/MainActivity.kt
+++ app/src/main/java/com/example/customview/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.customview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
